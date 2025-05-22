0dbd9af1c51abcb99e3fc35e3e412d25214d72f6
Luisaum2005 <luisotaviojassi2005@gmail.com>
2025-05-22 16:15:32 +0000

    .

--- src/main/java/application/controller/JogoController.java
+++ src/main/java/application/controller/JogoController.java
@@ -1,4 +1,26 @@
-package main.java.application.controller;
+package application.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import application.repository.GeneroRepository;
+import application.repository.JogoRepository;
+import application.repository.ModoRepository;
+import application.repository.PlataformaRepository;
+
+@Controller
+@RequestMapping("/jogos")
+public class JogoController {
+
+    @Autowired
+    private JogoRepository jogoRepo;
+    @Autowired
+    private ModoRepository modoRepo;
+    @Autowired
+    private GeneroRepository generoRepo;
+    @Autowired
+    private PlataformaRepository plataformaRepo;
+
+    
+}
